933b525cd5a4f4e2f607fd0ca4bb00a13fbea6a5
5-23 <yhanbyeol6@gmail.com>
2023-05-17 01:06:02 +0900

    make contact

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let a = 0
 window.onload = () =>{
     document.getElementById("before").addEventListener("click", () =>{
+        a = a > document.querySelectorAll(".work").length ? document.querySelectorAll(".work").length-1 : a
         if (0 > --a){ return }
+        document.getElementsByClassName("work")[a].className = "work active"
+        document.getElementsByClassName("work")[a + 1].className = "work"
+
+        
+        document.getElementsByClassName("item")[a].className = "item active"
+        document.getElementsByClassName("item")[a + 1].className = "item"
+
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*670}px)`
+            e.style.transform = `translateX(-${a*870}px)`
         })
     })
     
     document.getElementById("after").addEventListener("click", () =>{
+        a = a < 0? 0: a
         if (document.querySelectorAll(".work").length <= ++a){ return }
+        
         document.getElementsByClassName("work")[a - 1].className = "work"
         document.getElementsByClassName("work")[a].className = "work active"
+
+        document.getElementsByClassName("item")[a - 1].className = "item"
+        document.getElementsByClassName("item")[a].className = "item active"
+
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*670}px)`
+            e.style.transform = `translateX(-${a*870}px)`
         })
     })
 }
