7fb6935daeaf33a401ad000e60dbd46689bd9e89
5-23 dev <86705803+5-23@users.noreply.github.com>
2023-05-16 17:41:58 +0900

    asdf

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ window.onload = () =>{
     document.getElementById("before").addEventListener("click", () =>{
         if (0 > --a){ return }
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*650}px)`
+            e.style.transform = `translateX(-${a*670}px)`
         })
     })
     
     document.getElementById("after").addEventListener("click", () =>{
         if (document.querySelectorAll(".work").length <= ++a){ return }
+        document.getElementsByClassName("work")[a - 1].className = "work"
+        document.getElementsByClassName("work")[a].className = "work active"
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*650}px)`
+            e.style.transform = `translateX(-${a*670}px)`
         })
     })
 }
