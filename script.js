bd8ac78ca47c58ced2e24063a316d93365c265be
5-23 dev <86705803+5-23@users.noreply.github.com>
2023-05-17 08:33:54 +0900

    ㅁㄴㅇㄹ

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let a = 0
+des = ["대충설명1", "대충설명2", "대충설명3"]
 window.onload = () =>{
+    document.getElementById("des").innerHTML = des[0]
     document.getElementById("before").addEventListener("click", () =>{
         a = a > document.querySelectorAll(".work").length ? document.querySelectorAll(".work").length-1 : a
         if (0 > --a){ return }
         document.getElementsByClassName("work")[a].className = "work active"
         document.getElementsByClassName("work")[a + 1].className = "work"
-
+        document.getElementById("des").innerHTML = des[a]
+        
         
         document.getElementsByClassName("item")[a].className = "item active"
         document.getElementsByClassName("item")[a + 1].className = "item"
 
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*870}px)`
+            e.style.transform = `translateX(-${a*869}px)`
         })
     })
     
@@ -21,12 +24,14 @@ window.onload = () =>{
         
         document.getElementsByClassName("work")[a - 1].className = "work"
         document.getElementsByClassName("work")[a].className = "work active"
+        document.getElementById("des").innerHTML = des[a]
 
+        
         document.getElementsByClassName("item")[a - 1].className = "item"
         document.getElementsByClassName("item")[a].className = "item active"
 
         document.querySelectorAll(".work").forEach((e) => {
-            e.style.transform = `translateX(-${a*870}px)`
+            e.style.transform = `translateX(-${a*869}px)`
         })
     })
 }
